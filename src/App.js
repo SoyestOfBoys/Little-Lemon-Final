ca443da37befeac139211184796c1167fc21de1c
SoyestOfBoys <135057721+SoyestOfBoys@users.noreply.github.com>
2023-08-16 21:03:50 -0700

    fixed navigation issues, adding content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-function App() {
+import Home from './pages/Home';
+import About from './pages/About';
+import Login from './pages/Login';
+import Menu from './pages/Menu';
+import Order from './pages/Order';
+import Res from './pages/Reservations';
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path='login' element={<Login />}  />
+        <Route path='menu' element={<Menu />} />
+        <Route path='order' element={<Order />} />
+        <Route path='reservations' element={<Res />} />
+      </Routes>
     </div>
   );
 }
-
-export default App;
