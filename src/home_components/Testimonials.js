ffe0500a1d97df1d62b9a8cd2ba9cd13dcee0024
SoyestOfBoys <135057721+SoyestOfBoys@users.noreply.github.com>
2023-11-06 13:31:32 -0800

    CSS styling for home page

--- src/home_components/Testimonials.js
+++ src/home_components/Testimonials.js
@@ -9,23 +9,30 @@ function Testimonials() {
     <body className="testimonials">
       <h1>Testimonials</h1>
       <div className='TestiBox'>
-      <div className="testimonial 1">
-        <img className='Headshot' src={photo1} alt="Sophia Patel" />
-        <div className='NameBox'> <h3>Sophia Patel</h3>
-        <div className='Stars'><img className='Star' src={star} alt='star' /><img className='Star' src={star} alt='star' /><img className='Star' src={star} alt='star' /><img className='Star' src={star} alt='star' /><img className='Star' src={star} alt='star' /></div></div>
+        <div className="testimonial 1">
+          <div className='TestiTop'>
+            <img className='Headshot' src={photo1} alt="Sophia Patel" />
+            <div className='NameBox'>
+              <h3>Sophia Patel</h3>
+              <div className='Stars'>
+                <img className='Star' src={star} alt='star' /><img className='Star' src={star} alt='star' /><img className='Star' src={star} alt='star' /><img className='Star' src={star} alt='star' /><img className='Star' src={star} alt='star' />
+              </div>
+        </div></div>
         <p>"An impeccable fusion of flavors, cozy atmosphere, and impeccable presentation made this restaurant a new favorite in town!"</p>
       </div>
       <div className='testimonial 2'>
-        <img className='Headshot' src={photo2} alt='Allen Smithy' />
+      <div className='TestiTop'>
+        <img className='Headshot' src={photo3} alt='Allen Smithy' />
         <div className='NameBox'><h3>Allen Smithy</h3>
         <div className='Stars'><img className='Star' src={star} alt='star' /><img className='Star' src={star} alt='star' /><img className='Star' src={star} alt='star' /><img className='Star' src={star} alt='star' /><img className='Star' src={star} alt='star' /></div>
-        </div>
+        </div></div>
         <p>"A culinary delight! The chef's creative flair shines through in every dish, and the service was top-notch."</p>
       </div>
       <div className='testimonial 3'>
-        <img className='Headshot' src={photo3} alt='Emily Johnson' />
+      <div className='TestiTop'>
+        <img className='Headshot' src={photo2} alt='Emily Johnson' />
         <div className='NameBox'><h3>Emily Johnson</h3>
-        <div className='Stars'><img className='Star' src={star} alt='star' /><img className='Star' src={star} alt='star' /><img className='Star' src={star} alt='star' /><img className='Star' src={star} alt='star' /><img className='Star' src={star} alt='star' /></div></div>
+        <div className='Stars'><img className='Star' src={star} alt='star' /><img className='Star' src={star} alt='star' /><img className='Star' src={star} alt='star' /><img className='Star' src={star} alt='star' /><img className='Star' src={star} alt='star' /></div></div></div>
         <p>"From the moment we walked in, we were greeted with warmth and treated to an unforgettable gastronomic journey."</p>
       </div></div>
     </body>
